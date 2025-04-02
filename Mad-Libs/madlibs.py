f22a9788e268bbc287d4ec10a4e1505cfd942ceb
Kitzia Jazmin <78456393+7Kiin@users.noreply.github.com>
2025-04-02 17:55:54 -0600

    Upload file

---
+++ Mad-Libs/madlibs.py
@@ -0,0 +1,9 @@
+#mad Libs program with Python
+
+adjetive = input("Adjetive: ")
+verbOne = input("Verb: ")
+verbTwo = input("Verb: ")
+
+madlib = f"I have a monster pet, and it's so {adjetive}! He likes to eat bugs and boiled eggs, we like to {verbOne} together. Sometimes he likes to {verbTwo} when he is mad."
+
+print(madlib)
